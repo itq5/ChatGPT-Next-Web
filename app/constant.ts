8beb5442d4878f2f137477aa76118ab60c1ce89e
itq5 <42521985+itq5@users.noreply.github.com>
2023-11-08 23:38:50 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -141,6 +141,11 @@ export const DEFAULT_MODELS = [
     name: "gpt-3.5-turbo-16k-0613",
     available: true,
   },
+  // add models
+  {
+    name: "H0llyW00dzZ-1337-turbo-1337k-1337",
+    available: true,
+  },
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
